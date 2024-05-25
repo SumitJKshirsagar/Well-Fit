1dbd8247887f7607be7a52ab12030001376769b9
Swapnil0717 <pranavpathhare0987@gmail.com>
2024-05-25 15:50:07 +0530

    INITIAL COMMENT

--- app/src/main/java/com/example/well_fit/MainActivity.java
+++ app/src/main/java/com/example/well_fit/MainActivity.java
@@ -2,114 +2,172 @@ package com.example.well_fit;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.bumptech.glide.Glide;
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomsheet.BottomSheetDialog;
 import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
 import java.util.HashMap;
 import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-//    private static final int RC_SIGN_IN = 9001;
-//    private GoogleSignInClient mGoogleSignInClient;
-//    private FirebaseAuth mAuth;
-//    private FirebaseFirestore db;
-    Button login;
+    private static final int RC_SIGN_IN = 9001;
+    private GoogleSignInClient mGoogleSignInClient;
+    private FirebaseAuth mAuth;
+    private FirebaseFirestore db;
+
+    Button next;
+
+    ImageView i1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        login = findViewById(R.id.ls);
-//
-//        // Initialize Firebase Auth
-//        mAuth = FirebaseAuth.getInstance();
-//
-//        // Initialize Firestore
-//        db = FirebaseFirestore.getInstance();
-//
-//        // Configure Google Sign-In
-//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-//                .requestIdToken("175420533027-bo1bb61dg6b8mpm7249mnrsvu0as4g4s.apps.googleusercontent.com")
-//                .requestEmail()
-//                .build();
-//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        mAuth = FirebaseAuth.getInstance();
+        db = FirebaseFirestore.getInstance();
 
-        login.setOnClickListener(new View.OnClickListener() {
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("175420533027-bo1bb61dg6b8mpm7249mnrsvu0as4g4s.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        next = findViewById(R.id.next);
+        //i1 = findViewById ( R.id.i1 );
+
+        next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, Widget.class);
-                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                startActivity(intent);
+                BottomSheetDialog bottomSheetDialog = new BottomSheetDialog(MainActivity.this);
+                View view = LayoutInflater.from(MainActivity.this).inflate(R.layout.login_layout, null);
+                Button login = view.findViewById(R.id.login); // Assuming the TextView id is "text" in your layout
+                login.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        signIn();
+                    }
+                });
+                bottomSheetDialog.setContentView(view);
+                bottomSheetDialog.show();
             }
         });
+        //loadImageFromFirestore();
     }
 
-//    private void signIn() {
-//        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-//        startActivityForResult(signInIntent, RC_SIGN_IN);
-//    }
-//
-//    @Override
-//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-//        super.onActivityResult(requestCode, resultCode, data);
-//
-//        if (requestCode == RC_SIGN_IN) {
-//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
-//            try {
-//                GoogleSignInAccount account = task.getResult(ApiException.class);
-//                firebaseAuthWithGoogle(account);
-//            } catch (ApiException e) {
-//                // Handle the error
-//            }
-//        }
-//    }
-//
-//    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
-//        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
-//        mAuth.signInWithCredential(credential)
-//                .addOnCompleteListener(this, task -> {
-//                    if (task.isSuccessful()) {
-//                        FirebaseUser user = mAuth.getCurrentUser();
-//                        if (user != null) {
-//                            // Store user info in Firestore
-//                            Map<String, Object> userData = new HashMap<>();
-//                            userData.put("uid", user.getUid());
-//                            userData.put("name", user.getDisplayName());
-//                            userData.put("email", user.getEmail());
-//                            userData.put("photoUrl", user.getPhotoUrl().toString());
-//
-//                            db.collection("users").document(user.getUid())
-//                                    .set(userData)
-//                                    .addOnSuccessListener(aVoid -> {
-//                                        // Successfully stored user data
-//                                        Intent intent = new Intent(MainActivity.this, Widget.class);
-//                                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-//                                        startActivity(intent);
-//                                    })
-//                                    .addOnFailureListener(e -> {
-//                                        // Handle the error
-//                                        Toast.makeText ( this , e.getMessage () , Toast.LENGTH_SHORT ).show ( );
-//                                    });
-//                        }
-//                    } else {
-//                        // Handle sign in failure
-//                        Toast.makeText ( this , "Something went Wrong" , Toast.LENGTH_SHORT ).show ( );
-//                    }
-//                });
-//    }
+    /*private void loadImageFromFirestore ( ) {
+            db.collection("user_ui")
+                    .get()
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                if (document.contains("temp1")) {
+                                    String imageUrl = document.getString("temp1");
+                                    // Load the image using Glide
+                                    Glide.with(MainActivity.this)
+                                            .load(imageUrl)
+                                            .into(i1);
+                                }
+                            }
+                        } else {
+                            // Handle the error
+                        }
+                    });
+    }*/
+
+    private void signIn() {
+        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+        startActivityForResult(signInIntent, RC_SIGN_IN);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        // Result returned from launching the Intent from GoogleSignInClient.getSignInIntent(...);
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign-In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                firebaseAuthWithGoogle(account);
+            } catch (ApiException e) {
+                // Google Sign-In failed
+                Toast.makeText(MainActivity.this, "Google sign in failed", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void firebaseAuthWithGoogle(GoogleSignInAccount acct) {
+        AuthCredential credential = GoogleAuthProvider.getCredential(acct.getIdToken(), null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<com.google.firebase.auth.AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<com.google.firebase.auth.AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            // Update Firestore with the user's information
+                            storeUserInformation(user);
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(MainActivity.this, "Authentication Failed.", Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+                    }
+                });
+    }
+
+    private void storeUserInformation(FirebaseUser user) {
+        if (user != null) {
+            String uid = user.getUid();
+            String name = user.getDisplayName();
+            String email = user.getEmail();
+            String photoUrl = user.getPhotoUrl() != null ? user.getPhotoUrl().toString() : null;
+
+            Map<String, Object> userInfo = new HashMap<>();
+            userInfo.put("uid", uid);
+            userInfo.put("name", name);
+            userInfo.put("email", email);
+            userInfo.put("photoUrl", photoUrl);
+
+            DocumentReference userRef = db.collection("users").document(uid);
+            userRef.set(userInfo);
+        }
+    }
+
+    private void updateUI(FirebaseUser user) {
+        if (user != null) {
+            startActivity(new Intent(MainActivity.this, Widget.class));
+        } else {
+
+        }
+    }
+
 }
+
